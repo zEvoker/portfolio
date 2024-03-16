f5326008aab02a6ffc39a184d85cdbcdebb8f4fb
evoker <jbsimon03@gmail.com>
2024-03-16 10:28:09 +0530

    minor changes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,17 @@
 import { Link } from 'react-router-dom'
 import './index.scss'
 
-export const Card = ({ title, desc, imgUrl, lnk, longDesc, stack }) => {
+export const Card = ({ title, desc, imgUrl, lnk }) => {
     return (
-        <Link className="proj-bx" to={lnk} target='_blank'>
-            <div className="imgx">
-                <img src={imgUrl} alt="pic" />
-            </div>
-            <div className="dex">
-                <h2>{title}<br /><span>{desc}</span></h2>
-            </div>
-            <div className="descript">
-                <span>{longDesc}</span>
-            </div>
-            <div className='stack'>
-                <span>{stack}</span>
-                <button>Source Code</button>
-            </div>
-        </Link>
+        <>
+            <Link className="proj-bx" to={lnk} target='_blank'>
+                <div className="imgx">
+                    <img src={imgUrl} alt="pic" />
+                </div>
+                <div className="dex">
+                    <h2>{title}<br /><span>{desc}</span></h2>
+                </div>
+            </Link>
+        </>
     )
 }
